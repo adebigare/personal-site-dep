613340c5c6bb7cf816519db3df02fcbb08c7788c
Adrienne <atown@Adriennes-MacBook-Pro.local>
2018-05-03 14:38:16 -0400

    replacing require with gulp, removing photoswipe

--- dist/js/main.js
+++
@@ -1,8 +0,0 @@
-require(['nav','photoswipe-init'], function(nav, photoswipeInit) {
-
-    navBurgerCustom.addEventListener('click', nav.toggle);
-
-    document.getElementById('.my-gallery').addEventListener('click', initPhotoSwipeFromDOM('.my-gallery'));
-
-
-})
