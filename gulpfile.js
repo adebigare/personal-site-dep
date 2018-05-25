70b1d5c943143c7c7467d28a686a499a90cc9829
Adrienne <adrienne@OnoSendai.local>
2018-05-24 21:52:52 -0400

    Finally fixed gulp bugs w/Browsersync

--- gulpfile.js
+++ gulpfile.js
@@ -5,18 +5,28 @@ var uglify = require('gulp-uglify');
 var minifycss = require('gulp-minify-css');
 var concat = require('gulp-concat');
 var nunjucksRender = require('gulp-nunjucks-render');
+// var nunjucks = require('gulp-nunjucks');
 var data = require('gulp-data');
 var path = require('path');
 var fs = require('fs');
+var del = require('del');
+var watch = require('gulp-watch');
 
-// TO DO: create config file paths like in Pattern Lab
+var reload = browserSync.reload;
+
+gulp.task('clean', function(){
+  return del(['!dist', 'dist/styles', 'dist/js', 'dist/*.html']).then(paths => {
+    console.log('Deleted files and folders:\n', paths.join('\n'));
+  });
+});
+
 
 gulp.task('sass', function() {
   return gulp.src('src/scss/**/*.scss')
     .pipe(sass())
     .pipe(concat('style.css'))
     // .pipe(minifycss())
-    .pipe(gulp.dest('dist'))
+    .pipe(gulp.dest('dist/styles'))
     .pipe(browserSync.reload({
       stream: true
     }));
@@ -24,45 +34,57 @@ gulp.task('sass', function() {
 
 gulp.task('js', function() {
   return gulp.src('src/js/**/*.js')
-    .pipe(gulp.dest('dist'))
+    .pipe(gulp.dest('dist/js'))
     .pipe(browserSync.reload({
       stream: true
     }));
 });
+
+// gulp.task('nunjucks-precompile', function(){
+//   return gulp.src('./src/html/03-templates/*.+(html|nunjucks)')
+//     .pipe(nunjucks.precompile())
+//     .pipe(gulp.dest('dist'));
+
+// });
+
 
 gulp.task('nunjucks', function(){
-  return gulp.src('./src/html/02-organisms/.+(html|nunjucks)')
+  return gulp.src('./src/html/pages/**/*.+(html|nunjucks)')
     .pipe(data(function() {
-      return require('./src/data/global.json');
+      return JSON.parse(fs.readFileSync('./src/data/global.json'));
     }))
     .pipe(nunjucksRender({
-      path: 'src/html'
+      path: 'src/html/templates'
     }))
     .pipe(gulp.dest('dist'))
-    .pipe(browserSync.reload({
+    .pipe(reload({
       stream: true
     }));
 });
 
-gulp.task('browserSync', function() {
+gulp.task('serve', ['build'], function() {
   browserSync.init({
     server: {
       baseDir: 'dist'
-    },
+    }
   });
+
 });
 
-gulp.task('build', ['browserSync','sass', 'nunjucks', 'js']);
+
+gulp.task('watch', function (){
+  gulp.watch('html/**/*.+(html|nunjucks)', {cwd: 'src'}, ['nunjucks']);
+  gulp.watch('scss/**/*.scss ', {cwd: 'src'}, ['sass']);
+  gulp.watch('js/**/*.js ', {cwd: 'src'}, ['js']);
+});
+
+
+gulp.task('build', ['sass', 'nunjucks', 'js']);
 
 // ***************************************
 
-gulp.task('watch', ['browserSync', 'nunjucks', 'sass', 'js'], function (){
-  gulp.watch('src/**/*.+(html|nunjucks)', ['nunjucks']);
-  gulp.watch('src/scss/**/*.scss ', ['sass']);
-  gulp.watch('src/js/**/*.js ', ['js']);
-  // gulp.watch('src/js/**/*.js', browserSync.reload);
-});
+
 
 // *******************************************
 
-gulp.task('default', ['watch']);
+gulp.task('default', ['serve','watch']);
